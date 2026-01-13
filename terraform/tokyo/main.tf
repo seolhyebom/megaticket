f0270144bc0ad20ad8cc9c6a3dc91e5b04305087
playdelaybluelay-stack <playdelaybluelay@gmail.com>
2026-01-13 14:12:53 +0900

    fix route table reference

--- terraform/tokyo/main.tf
+++ terraform/tokyo/main.tf
@@ -182,7 +182,7 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.main.id
   service_name      = "com.amazonaws.${var.aws_region}.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids = var.private_route_table_ids 
+  route_table_ids = [aws_route_table.private.id]
 
   policy = jsonencode({
     Version = "2012-10-17"
