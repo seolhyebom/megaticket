6e204d74b017def12b726769ac709318d2b55c0e
Shinsungwoo21 <tlstjd1007@naver.com>
2026-01-15 15:01:09 +0900

    API_URL 값 변경함

--- apps/web/public/config.js
+++ apps/web/public/config.js
@@ -1,7 +1,7 @@
 // public/config.js
 // 서울 리전 기본 설정 - 배포 시 리전별로 다른 파일 사용
 window.__PLCR_CONFIG__ = {
-  API_URL: "https://api.pilotlight-test.click",
+  API_URL: "https://app.pilotlight-test.click",
   AWS_REGION: "ap-southeast-1",
   PROJECT: "plcr",
   ENVIRONMENT: "prod",
